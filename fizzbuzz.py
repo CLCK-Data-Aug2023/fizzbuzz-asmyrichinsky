1eb6540e347cd6a06b1565d6013210591388ffaa
asmyrichinsky <142461542+asmyrichinsky@users.noreply.github.com>
2023-10-25 19:38:28 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,14 @@
-# add your code here
+# Write a Python program that prints the numbers from 1 to 100. If the number is divisible by 3 print Fizz, if the number is divisible by 5 print Buzz instead of the number. 
+# Write a program that prints the numbers from 1 to 100.
+# If the number is divisible by 3 print Fizz
+# If the number is divisible by 5 print Buzz
+# If the number is divisible by 3 and 5 print FizzBuzz instead of the number.
+def fizz_buzz(input):
+  if input% 3 == 0 and input % ==  0:
+    return 'FizzBuzz'
+  if input % 3 == 0:
+    return "Fizz"
+  if input % 5 ==0:
+    return 'Buzz'
 
+  return input
